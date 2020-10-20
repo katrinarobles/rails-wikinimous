d79cc4a97a4f0ccae635f5ae74bc6336fc17bc09
Katrina Robles <katrinarobles@live.com>
2020-10-20 16:47:49 +0200

    Wikinimous

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,14 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+require "faker"
+# TODO: Write a seed to insert 100 posts in the database
+puts 'Creating 10 fake articles...'
+10.times do
+  article = Article.new(
+    title: Faker::Lorem.sentence(word_count: 3).to_s,
+    content: Faker::ChuckNorris.fact
+  )
+  article.save!
+end
+puts 'Finished!'
